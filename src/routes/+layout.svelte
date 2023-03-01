199611b650b7e7ab246ae4af1c506f7ef4b330a4
Szymon Sadowski <szymon.sadowski@boldare.com>
2023-03-01 18:24:33 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="max-w-4xl mx-auto w-full p-6">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+
+<h1 class="text-center font-extrabold text-black text-3xl md:text-5xl mb-10">Welcome to SvelteKit</h1>
+
+<div class="bg-red w-full h-4">
+	test
+</div>
